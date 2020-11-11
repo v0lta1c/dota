f9d19d7f08928494c7cf19cafac228c3f36f0ce6
v0lta1c <mann.bilimoria@gmail.com>
2020-11-11 14:15:01 +0530

    Some changes

--- apiCalls.py
+++ apiCalls.py
@@ -13,36 +13,17 @@ def generateItemList(token):
 	URL = 'http://api.steampowered.com/IEconDOTA2_570/GetGameItems/v1?key=' + token
 	items = requests.get(url = URL)
 	matches = requests.get(url = OPENDOTA)
-	#content = json.loads(items.content.decode('utf-8').replace('\n', '\n'))
-	#content = json.loads(items.content)
+	content = json.loads(items.content.decode('utf-8').replace('\n', ''))
 
-	#outfile = open("shop_items.json",'w')
-	#outfile.write(json.dumps(items.content))
-	#outfile.close
-	#json.dump(content,outfile)
-	#outfile.close
-	#sorting your json output by keys so that you get your output as {"1":{}, "2":{}...}
-	results_str = json.dumps(items.content.decode('utf-8').replace('\n', ''), sort_keys=True)
 
-	#removing the outer brackets {}, now the results_str would look something like this "1":{}, "2":{}...
-	results_str = results_str[1:-1]
+	outfile = open("shop_items.json",'w')
 
-	#splitting the string by "}," as delimiter, so results_list would look like this ["1":{, "2":{, "3":{}]
-	results_list = results_str.split("},")
+	json.dump(content,outfile)
+	outfile.close
 
-	#print the results to the file as per the desired format
-	with open('shop_items.json', 'w') as outfile:
-	    outfile.write('\n')
-	    for p in results_list[:-1]:
-	        print (p+'}', file=outfile)
-	    print (results_list[-1], file=outfile)
 
 	with open('matches.txt','w') as file:
 		file.write(matches.content.decode());
-	#with fileinput.FileInput("shop_items.json",inplace=True,backup='.bak') as file:
-		#for line in file:
-			#print(line.replace('\n','\\n'), end='')
-
 
 
 generateItemList(STEAM_AUTH_TOKEN)
